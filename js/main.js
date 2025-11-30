d701d54493d05959105010039903870e5418d4dc
Kentaa Kakashi <168970257+Kentaakakashi@users.noreply.github.com>
2025-11-30 15:47:01 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+alert("✅ JS Loaded");
 
+document.addEventListener("DOMContentLoaded", function () {
+  var btn = document.getElementById("startBtn");
+
+  if (!btn) {
+    alert("❌ Button not found in index.html");
+    return;
+  }
+
+  btn.addEventListener("click", function () {
+    alert("✅ Button clicked, going to hub");
+    window.location.href = "hub.html";
+  });
+});
